23aa18bcd2a6c2d29dbefb68dbe278b840688f5f
Praveen <er.praveendewangan@gmail.com>
2020-11-22 10:19:08 +0530

    working on Browse page

--- netflix/src/app.js
+++ netflix/src/app.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home , Browse, Signin, Signup } from './pages';
 import { IsUserRedirect , ProtectedRoute } from './helpers/routes';
+import { useAuthListener } from './hooks';
 function App() {
-  const user = null;
+  const user = useAuthListener();
+  console.log(user);
   return (
     <Router>
       <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
--- netflix/src/pages/browse.js
+++ netflix/src/pages/browse.js
@@ -1,6 +1,12 @@
 import React from 'react';
- 
+import { BrowseContainer } from '../containers/browse';
+import { useContent } from '../hooks'; 
+import selectionFilter from '../utils/selection-filter';
 export default function Browse () {
-      return (<p>Hello browse</p>);
+      const { series } = useContent('series')
+      const { films } = useContent('films')
+      const slides = selectionFilter({series,films});
+      console.log(slides)
+      return <BrowseContainer slides={slides}/>
 };
  
